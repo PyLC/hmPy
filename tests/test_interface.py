d4b3b8db01a1b529d1dc2f7d61b36e37eba63d62
jseed <jonathan.j.seed@gmail.com>
2017-09-28 15:57:30 -0400

    Add methods for scheduling actions to Interface

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -4,12 +4,29 @@ from hmpy import Interface
 
 @pytest.fixture(scope="module")
 def interface():
-    intf = Interface()
-    yield intf
-    intf = None
+    return Interface()
 
 
 def test_multiple_instances(interface):
-    """Test trying to create multiple instances of class Interface"""
+    # Test trying to create multiple instances of class Interface
     with pytest.raises(Exception):
         int2 = Interface()
+
+
+def test_after(interface, qtbot, mocker):
+    # Test that the .after method executes the action only once,
+    # and after the expected delay
+    action = mocker.stub()
+    interface.after(10, action)
+    action.assert_not_called()
+    qtbot.wait(25)
+    action.assert_called_once()
+
+
+def test_every(interface, qtbot, mocker):
+    # Test that the .every method executes the action at the expected interval
+    action = mocker.stub()
+    interface.every(100, action)
+    action.assert_not_called()
+    qtbot.wait(250)
+    assert action.call_count == 2
