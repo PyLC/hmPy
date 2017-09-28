6386c6f7825d166d30f6a42373ef25d490694461
jseed <jonathan.j.seed@gmail.com>
2017-09-28 14:24:35 -0400

    Add LCDView class for displaying numbers

--- hmpy/views/__init__.py
+++ hmpy/views/__init__.py
@@ -1,5 +1,5 @@
-
 from hmpy.views.button import ButtonView
+from hmpy.views.lcd import LCDView
 from hmpy.views.view import View
 
-__all__ = ["ButtonView", "View"]
+__all__ = ["ButtonView", "LCDView", "View"]
