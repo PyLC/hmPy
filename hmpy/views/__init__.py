a1fd4b42206ed056d835f713c99e87f669936424
jseed <jonathan.j.seed@gmail.com>
2017-10-31 16:30:42 -0400

    Add LEDView class for displaying Boolean state

--- hmpy/views/__init__.py
+++ hmpy/views/__init__.py
@@ -1,5 +1,6 @@
 from hmpy.views.button import ButtonView
 from hmpy.views.lcd import LCDView
+from hmpy.views.led import LEDView
 from hmpy.views.view import View
 
-__all__ = ["ButtonView", "LCDView", "View"]
+__all__ = ["ButtonView", "LCDView", "LEDView", "View"]
