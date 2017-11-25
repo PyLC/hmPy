50b72c321dde287eee784d12fb7f6397afe2d849
kody91 <emm00001@algonquinlive.com>
2017-11-25 13:41:06 -0500

    Modified line chart into an area chart, as per client requirements

--- hmpy/views/LineChart.py
+++ hmpy/views/LineChart.py
@@ -1,5 +1,5 @@
-from PyQt5.QtChart import QChart, QChartView, QLineSeries, QValueAxis
-from PyQt5.QtGui import QPainter
+from PyQt5.QtChart import QChart, QChartView, QLineSeries, QValueAxis, QAreaSeries
+from PyQt5.QtGui import QPainter, QLinearGradient
 from PyQt5.QtCore import Qt, QPointF
 from hmpy.views.view import View
 
@@ -20,9 +20,9 @@ class LineChart(View):
         self.chart.setTitle(title)
 
     def add_line(self, line):
-        self.chart.addSeries(line.get_internal_line())
-        self.chart.setAxisX(line.x_axis, line.get_internal_line())
-        self.chart.setAxisY(line.y_axis, line.get_internal_line())
+        self.chart.addSeries(line.get_area_line())
+        self.chart.setAxisX(line.x_axis, line.get_area_line())
+        self.chart.setAxisY(line.y_axis, line.get_area_line())
 
     def paintEvent(self, event):
         size = event.rect().size()
@@ -43,12 +43,19 @@ class Line:
     yellow = Qt.yellow
 
     def __init__(self, colour=None, width=0.1):
-        self._line = QLineSeries()
-        self.pen = self._line.pen()
+        self._line_actual = QLineSeries()
+        self._line_area = QLineSeries()
+        self._area_series = QAreaSeries(self._line_actual, self._line_area)
+        self.pen = self._area_series.pen()
+        self._gradient = QLinearGradient(QPointF(0, 0), QPointF(0, 1))
         if colour is not None:
             self.pen.setColor(colour)
+            self._gradient.setColorAt(0.0, colour)
+            self._gradient.setColorAt(1.0, colour)
+            self._gradient.setCoordinateMode(QLinearGradient.ObjectBoundingMode)
         self.pen.setWidth(width)
-        self._line.setPen(self.pen)
+        self._line_actual.setPen(self.pen)
+        self._area_series.setBrush(self._gradient)
         self.y_axis = QValueAxis()
         self.x_axis = QValueAxis()
         self.y_axis.alignment = Qt.AlignLeft
@@ -63,20 +70,25 @@ class Line:
         if isinstance(x_data, list) and isinstance(y_data, list):
             if len(x_data) == len(y_data):
                 for i in range(len(x_data)):
-                    self._line.append(x_data[i], y_data[i])
+                    self._line_actual.append(x_data[i], y_data[i])
+                    self._line_area.append(x_data[i], 0)  # keep a second line at the X-axis
             else:
                 raise Exception("X and Y data lists do not have equal amount of elements")
         else:
-            self._line.append(x_data, y_data)
-        if self._line.count() > 100:
-            self._line.removePoints(0, self._line.count() - 100)
+            self._line_actual.append(x_data, y_data)
+            self._line_area.append(x_data, 0)
+        if self._line_actual.count() > 100:
+            self._line_actual.removePoints(0, self._line_actual.count() - 100)
+            self._line_area.removePoints(0, self._line_actual.count() - 100)
         self.y_axis.setRange(self._get_y_limits()[0], self._get_y_limits()[1])
         self.x_axis.setRange(self._get_x_limits()[0], self._get_x_limits()[1])
+
     """
-        :returns QLineSeries representation of the line
+        :returns the area chart itself
     """
-    def get_internal_line(self):
-        return self._line
+
+    def get_area_line(self):
+        return self._area_series
 
     """
         :returns tuple, first element being the minimum Y value in the series, second element is the max Y value in the
@@ -84,7 +96,7 @@ class Line:
     """
     def _get_y_limits(self):
         list_of_y_points = []
-        point_list = self._line.pointsVector()
+        point_list = self._line_actual.pointsVector()
         for i in point_list:
             # TODO: there needs to be a better way to do this
             list_of_y_points.append(i.y())
@@ -95,7 +107,7 @@ class Line:
     """
     def _get_x_limits(self):
         list_of_x_points = []
-        point_list = self._line.pointsVector()
+        point_list = self._line_actual.pointsVector()
         for i in point_list:
             # TODO: Better way to do this
             list_of_x_points.append(i.x())
