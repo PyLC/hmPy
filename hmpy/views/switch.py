c603b3ccd2c52013397c74d1db5d1ea07a1610a5
davidjoseph9 <davidjos3ph94@gmail.com>
2017-11-26 18:00:48 -0500

    Uses pixmap now

--- hmpy/views/switch.py
+++ hmpy/views/switch.py
@@ -1,16 +1,15 @@
 """This module defines the ButtonView class."""
 
 
-from .view import View
-from PyQt5.QtWidgets import QPushButton
-from PyQt5.QtGui import QPainter, QBrush, QPen, QColor
-from PyQt5.QtCore import Qt, QPoint
+from hmpy.views.view import View
+from PyQt5.QtGui import QPainter, QImage, QPixmap
+from PyQt5.QtCore import Qt
 
 
 class SwitchView(View):
     """The Switch view creates a clickable switch for triggering actions."""
 
-    _SCALE = 150
+    _SCALE = 100
 
     def __init__(self, on=False):
         """Initialize the switch view.
@@ -21,35 +20,39 @@ class SwitchView(View):
         self.mousePressEvent = self._mouse_press_event
         self._on_toggle = None
 
+        # Since there is only two states, cache them, instead of repainting on change
+        self._pixmap_cache = {}
+
     def paintEvent(self, event):
+        """Draw the Switch graphic
+
+        :param event: The paint event.
+        """
+
+        # Attempt to fetch the cached pixmap for the current state
+        image = self._pixmap_cache.get(self.on, None)
+
+        if image is None:
+            # If a cached pixmap is not set, generate one and cache it
+            image = self._generate_pixmap(event.rect())
+            self._pixmap_cache[self.on] = image
+
+        # Draw image to view
         paint = QPainter()
         paint.begin(self)
-
-        paint.setBrush(Qt.lightGray)
-        scale_x = self.width() / self._SCALE
-        scale_y = self.height() / self._SCALE
-        paint.drawRect(0, 0, 95*scale_x, 45*scale_y)
-
-        paint.setBrush(Qt.darkGray)
-        paint.drawRect(10 * scale_x, 10 * scale_y, 80 * scale_x, 25 * scale_y)
-
-        if self._state:
-            paint.setBrush(Qt.green)
-            paint.drawRect(50 * scale_x, 10 * scale_y, 40 * scale_x, 25 * scale_y)
-        else:
-            paint.setBrush(Qt.red)
-            paint.drawRect(10 * scale_x, 10 * scale_y, 40 * scale_x, 25 * scale_y)
-
+        paint.drawPixmap(self.rect(), image)
         paint.end()
+
 
     @property
     def on(self):
-        self.repaint()
         return self._state
 
     @on.setter
     def on(self, to):
         self._state = to
+
+        self.repaint()
 
     def toggle(self):
         self._state = not self._state
@@ -65,3 +68,37 @@ class SwitchView(View):
     def _mouse_press_event(self, event):
         self.toggle()
 
+    def _generate_pixmap(self, rect):
+        """Generate the pixmap representing the LED's current state
+        :param rect: the QRectangle representing the view.
+        :return:
+        """
+        paint = QPainter()
+
+        # Get view size and center
+        size = rect.size()
+
+        # Draw graphics to image (double buffer)
+        image = QImage(size, QImage.Format_ARGB32_Premultiplied)
+        image.fill(0)
+
+        paint.begin(image)
+
+        paint.setBrush(Qt.lightGray)
+        scale_x = self.width() / self._SCALE
+        scale_y = self.height() / self._SCALE
+        paint.drawRect(0, 0, 95 * scale_x, 45 * scale_y)
+
+        paint.setBrush(Qt.darkGray)
+        paint.drawRect(10 * scale_x, 10 * scale_y, 80 * scale_x, 25 * scale_y)
+
+        if self._state:
+            paint.setBrush(Qt.green)
+            paint.drawRect(50 * scale_x, 10 * scale_y, 40 * scale_x, 25 * scale_y)
+        else:
+            paint.setBrush(Qt.red)
+            paint.drawRect(10 * scale_x, 10 * scale_y, 40 * scale_x, 25 * scale_y)
+        paint.end()
+
+        # Convert to pixmap and return
+        return QPixmap.fromImage(image)
