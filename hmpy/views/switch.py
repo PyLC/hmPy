06096bcd21614e98ff7bb81570f8b35525d511bd
davidjoseph9 <davidjos3ph94@gmail.com>
2017-11-28 21:58:16 -0500

    Visual changes and pep8 changes

--- hmpy/views/switch.py
+++ hmpy/views/switch.py
@@ -1,8 +1,8 @@
-"""This module defines the ButtonView class."""
+"""This module defines the SwitchView class."""
 
 
 from hmpy.views.view import View
-from PyQt5.QtGui import QPainter, QImage, QPixmap
+from PyQt5.QtGui import QPainter, QImage, QPixmap, QConicalGradient, QPalette
 from PyQt5.QtCore import Qt
 
 
@@ -20,7 +20,6 @@ class SwitchView(View):
         self.mousePressEvent = self._mouse_press_event
         self._on_toggle = None
 
-        # Since there is only two states, cache them, instead of repainting on change
         self._pixmap_cache = {}
 
     def paintEvent(self, event):
@@ -43,7 +42,6 @@ class SwitchView(View):
         paint.drawPixmap(self.rect(), image)
         paint.end()
 
-
     @property
     def on(self):
         return self._state
@@ -51,15 +49,12 @@ class SwitchView(View):
     @on.setter
     def on(self, to):
         self._state = to
-
         self.repaint()
 
     def toggle(self):
         self._state = not self._state
-
         if self._on_toggle is not None:
             self._on_toggle()
-
         self.repaint()
 
     def on_toggle(self, callback):
@@ -84,20 +79,36 @@ class SwitchView(View):
 
         paint.begin(image)
 
-        paint.setBrush(Qt.lightGray)
+        # Set the border gradient
+        border_gradient = QConicalGradient(rect.center(), -30)
+        border_color = self.palette().color(QPalette.Dark)
+        border_gradient.setColorAt(0.2, border_color)
+        border_gradient.setColorAt(0.5, self.palette().color(QPalette.Light))
+        border_gradient.setColorAt(0.8, border_color)
+
+        paint.setBrush(border_gradient)
         scale_x = self.width() / self._SCALE
         scale_y = self.height() / self._SCALE
-        paint.drawRect(0, 0, 95 * scale_x, 45 * scale_y)
 
+        paint.drawRect(0, 0, 95 * scale_x, 45 * scale_y)
         paint.setBrush(Qt.darkGray)
-        paint.drawRect(10 * scale_x, 10 * scale_y, 80 * scale_x, 25 * scale_y)
+        paint.drawRect(8 * scale_x, 10 * scale_y, 80 * scale_x, 25 * scale_y)
 
         if self._state:
             paint.setBrush(Qt.green)
-            paint.drawRect(50 * scale_x, 10 * scale_y, 40 * scale_x, 25 * scale_y)
+            paint.drawRect(48 * scale_x, 10 * scale_y,
+                           40 * scale_x, 25 * scale_y)
+            paint.setBrush(Qt.darkGreen)
+            paint.drawRect(80 * scale_x, 10 * scale_y,
+                           8 * scale_x, 25 * scale_y)
         else:
             paint.setBrush(Qt.red)
-            paint.drawRect(10 * scale_x, 10 * scale_y, 40 * scale_x, 25 * scale_y)
+            paint.drawRect(8 * scale_x, 10 * scale_y,
+                           40 * scale_x, 25 * scale_y)
+            paint.setBrush(Qt.darkRed)
+            paint.drawRect(8 * scale_x, 10 * scale_y,
+                           8 * scale_x, 25 * scale_y)
+
         paint.end()
 
         # Convert to pixmap and return
