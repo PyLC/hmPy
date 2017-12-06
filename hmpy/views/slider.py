e16d1cb98e10882d80bb7d3e9e3676c6f75d88ee
lafo0203 <lafo0203@algonquinlive.com>
2017-12-05 20:39:19 -0500

    Slider component, still need to complete test

--- hmpy/views/slider.py
+++ hmpy/views/slider.py
@@ -4,27 +4,65 @@
 from .view import View
 from PyQt5.QtWidgets import QSlider,QLabel
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter
 
 class SliderView(View):
-    """The Slider view creates a slider for input."""
+    """The Slider view creates a slider for input.
 
-    def __init__(self,text,max,min,cur):
-        """Initialize the slider view.
+    :param minimum: minimum int value of slider.
+    :param maximum: maximum int value of slider.
+    :param interval: distance each tick will travel on slider.
+    """
+    def __init__(self, minimum=0, maximum=100,interval=1):
+        """Initialize the slider view."""
 
-        :param text: The text to display on the slider"""
         super().__init__()
-        self.slider = QSlider()
-        self.slider.setRange(min, max)
-        self.slider.setValue(cur)
-        self.slider.valueChanged.connect(self.slider_changed)
-        self.text = text;
+        self.slider = QSlider(self)
+        self.slider.setMinimum(minimum)
+        self.slider.setMaximum(maximum)
+        self.slider.setTickInterval(interval)
 
 
     @property
-    def text(self):
-        self.slider.text()
+    def tick_interval(self):
+        return self.slider.tickInterval()
 
-    @text.setter
-    def text(self, text):
-        self.slider.setText(text)
+    @tick_interval.setter
+    def tick_interval(self,interval):
+        if interval <= 0:
+            self.slider.setTickInterval(self.slider.tickInterval())
+        else:
+            self.slider.setTickInterval(interval)
+
+    @property
+    def maximum_value(self):
+        return self.slider.maximum()
+
+    @maximum_value.setter
+    def maximum_value(self,maximum):
+        if maximum < 0:
+            self.slider.setMaximum(self.slider.maximum())
+        else:
+            self.slider.setMaximum(maximum)
+
+    @property
+    def minimum_value(self):
+        return self.slider.minimum()
+
+    @minimum_value.setter
+    def minimum_value(self, minimum):
+        if minimum >= 0:
+            self.slider.setMinimum(minimum)
+        else:
+            self.slider.setMinimum(0)
+
+    def set_horizontal_orientation(self):
+        self.slider.setOrientation(Qt.Horizontal)
+
+    def set_vertical_orientation(self):
+        self.slider.setOrientation(Qt.Vertical)
+
+
+
+
 
