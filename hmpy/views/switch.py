6340cde44369ffa7c35ead91b26a2a520c98759e
jose0144 <jose0144@algonquinlive.com>
2017-12-09 17:41:52 -0500

    Changes after code review

--- hmpy/views/switch.py
+++ hmpy/views/switch.py
@@ -2,117 +2,146 @@
 
 
 from hmpy.views.view import View
-from PyQt5.QtGui import QPainter, QImage, QPixmap, QConicalGradient, QPalette
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QImage, QPixmap, QConicalGradient, \
+    QPalette
+from PyQt5.QtWidgets import QAbstractButton
+from PyQt5.QtCore import Qt, QSize
 
 
 class SwitchView(View):
     """The Switch view creates a clickable switch for triggering actions."""
 
-    _SCALE = 100
+    class _SwitchButton(QAbstractButton):
+        """Switch button component"""
+        _SCALE = 100
 
-    def __init__(self, on=False):
+        def __init__(self, text, parent):
+            QAbstractButton.__init__(self, parent=parent)
+            self.setCheckable(True)
+            self._text = text
+            self._pixmap_cache = {}
+
+        def paintEvent(self, event):
+            """Draw the Switch graphic"""
+            # Attempt to fetch the cached pixmap for the current state
+            image = self._pixmap_cache.get(self.isChecked(), None)
+
+            if image is None:
+                # If a cached pixmap is not set, generate one and cache it
+                image = self._generate_pixmap(event.rect())
+
+                self._pixmap_cache[self.isChecked()] = image
+
+            # Draw image to view
+            paint = QPainter()
+            paint.begin(self)
+            paint.drawPixmap(self.rect(), image)
+            paint.end()
+
+        def _generate_pixmap(self, rect):
+            """Generate the pixmap representing the Switch's current state
+            :param rect: the QRectangle representing the view.
+            :return:
+            """
+            paint = QPainter()
+
+            # Get view size and center
+            size = rect.size()
+
+            # Draw graphics to image (double buffer)
+            image = QImage(size, QImage.Format_ARGB32_Premultiplied)
+            image.fill(0)
+
+            paint.begin(image)
+            paint.translate(self.width() / 4, 0)
+
+            # Set the border gradient
+            border_gradient = QConicalGradient(rect.center(), -30)
+            border_color = self.palette().color(QPalette.Dark)
+            border_gradient.setColorAt(0.2, border_color)
+            border_gradient.setColorAt(0.5,
+                                       self.palette().color(QPalette.Light))
+            border_gradient.setColorAt(0.8, border_color)
+
+            paint.setBrush(border_gradient)
+            scale_x = self.width() / self._SCALE
+            scale_y = self.height() / self._SCALE
+            paint.drawRect(-20 * scale_x, 0,
+                           85 * scale_x, 85 * scale_y)
+
+            paint.setBrush(Qt.darkGray)
+            paint.drawRect(-5 * scale_x, 10 * scale_y,
+                           55 * scale_x, 70 * scale_y)
+
+            if self.isChecked():
+                paint.setBrush(Qt.green)
+                paint.drawRect(-5 * scale_x, 10 * scale_y,
+                               55 * scale_x, 35 * scale_y)
+                paint.setBrush(Qt.darkGreen)
+                paint.drawRect(-5 * scale_x, 8 * scale_y,
+                               55 * scale_x, 10 * scale_y)
+            else:
+                paint.setBrush(Qt.red)
+                paint.drawRect(-5 * scale_x, 45 * scale_y,
+                               55 * scale_x, 35 * scale_y)
+
+                paint.setBrush(Qt.darkRed)
+                paint.drawRect(-5 * scale_x, 70 * scale_y,
+                               55 * scale_x, 10 * scale_y)
+
+            paint.save()
+
+            paint.translate(self.width() / 2, 35 * scale_y)
+
+            paint.setBrush(Qt.darkGray)
+            paint.drawRect(-70 * scale_x, 50 * scale_y,
+                           85 * scale_x, 12 * scale_y)
+
+            paint.setPen(Qt.white)
+
+            paint.drawText(-55 * scale_x, 58 * scale_y, self._text)
+
+            paint.restore()
+            paint.end()
+
+            # Convert to pixmap and return
+            return QPixmap.fromImage(image)
+
+    def __init__(self, text, on=False):
         """Initialize the switch view.
 
         :param on: the state of the button"""
         super().__init__()
-        self._state = on
-        self.mousePressEvent = self._mouse_press_event
-        self._on_toggle = None
-
-        self._pixmap_cache = {}
-
-    def paintEvent(self, event):
-        """Draw the Switch graphic
-
-        :param event: The paint event.
-        """
-
-        # Attempt to fetch the cached pixmap for the current state
-        image = self._pixmap_cache.get(self.on, None)
-
-        if image is None:
-            # If a cached pixmap is not set, generate one and cache it
-            image = self._generate_pixmap(event.rect())
-            self._pixmap_cache[self.on] = image
-
-        # Draw image to view
-        paint = QPainter()
-        paint.begin(self)
-        paint.drawPixmap(self.rect(), image)
-        paint.end()
+        self._button = self._SwitchButton(text, self)
+        self._button.setChecked(on)
 
     @property
     def on(self):
-        return self._state
+        return self._button.isChecked()
 
     @on.setter
-    def on(self, to):
-        self._state = to
-        self.repaint()
-
-    def toggle(self):
+    def on(self, state):
         """
-        Toggle the state of the switch
-        """
-        self._state = not self._state
-        if self._on_toggle is not None:
-            self._on_toggle()
-        self.repaint()
+        Set the button state
 
-    def on_toggle(self, callback):
-        self._on_toggle = callback
-
-    def _mouse_press_event(self, event):
-        self.toggle()
-
-    def _generate_pixmap(self, rect):
-        """Generate the pixmap representing the LED's current state
-        :param rect: the QRectangle representing the view.
+        :param state:
         :return:
         """
-        paint = QPainter()
+        if state != self._button.isChecked():
+            self._button.toggle()
 
-        # Get view size and center
-        size = rect.size()
+    def toggle(self):
+        self._button.toggle()
 
-        # Draw graphics to image (double buffer)
-        image = QImage(size, QImage.Format_ARGB32_Premultiplied)
-        image.fill(0)
+    def on_toggle(self, callback):
+        """Set callback for on click"""
+        if callable(callback):
+            self._button.toggled.connect(callback)
 
-        paint.begin(image)
-
-        # Set the border gradient
-        border_gradient = QConicalGradient(rect.center(), -30)
-        border_color = self.palette().color(QPalette.Dark)
-        border_gradient.setColorAt(0.2, border_color)
-        border_gradient.setColorAt(0.5, self.palette().color(QPalette.Light))
-        border_gradient.setColorAt(0.8, border_color)
-
-        paint.setBrush(border_gradient)
-        scale_x = self.width() / self._SCALE
-        scale_y = self.height() / self._SCALE
-
-        paint.drawRect(0, 0, 95 * scale_x, 45 * scale_y)
-        paint.setBrush(Qt.darkGray)
-        paint.drawRect(8 * scale_x, 10 * scale_y, 80 * scale_x, 25 * scale_y)
-
-        if self._state:
-            paint.setBrush(Qt.green)
-            paint.drawRect(48 * scale_x, 10 * scale_y,
-                           40 * scale_x, 25 * scale_y)
-            paint.setBrush(Qt.darkGreen)
-            paint.drawRect(80 * scale_x, 10 * scale_y,
-                           8 * scale_x, 25 * scale_y)
-        else:
-            paint.setBrush(Qt.red)
-            paint.drawRect(8 * scale_x, 10 * scale_y,
-                           40 * scale_x, 25 * scale_y)
-            paint.setBrush(Qt.darkRed)
-            paint.drawRect(8 * scale_x, 10 * scale_y,
-                           8 * scale_x, 25 * scale_y)
-
-        paint.end()
-
-        # Convert to pixmap and return
-        return QPixmap.fromImage(image)
+    def paintEvent(self, event):
+        """Draw the Switch graphic onto the view"""
+        rect = self.rect()
+        size = QSize(rect.width() * .65,
+                     rect.height() * .75)
+        if size != self._button.size():
+            self._button.resize(size)
--- tests/views/test_switch.py
+++ tests/views/test_switch.py
@@ -4,37 +4,45 @@ from PyQt5.QtCore import Qt
 
 
 def test_on_init(qtbot):
-    # Test that the value is initialized properly
-    switch = SwitchView(False)
+    switch = SwitchView("", False)
+
     qtbot.addWidget(switch)
     assert not switch.on
 
 
 def test_on_set(qtbot):
-    # Test that the value is set properly
-    switch = SwitchView()
+    switch = SwitchView("")
     switch.on = True
+
     qtbot.addWidget(switch)
     assert switch.on is True
 
 
-def test_toggle(qtbot):
-    # Test that the switch on state toggles properly
-    switch = SwitchView(False)
-    # toggle on
+def test_toggle_on_checked(qtbot):
+    switch = SwitchView("", False)
+
     switch.toggle()
     qtbot.addWidget(switch)
     assert switch.on is True
+
+
+def test_toggle_on_toggle(qtbot, mocker):
+    mock = mocker.stub()
+    switch = SwitchView("", False)
+    switch.on_toggle(mock)
+
     switch.toggle()
-    assert switch.on is False
+    qtbot.addWidget(switch)
+
+    mock.assert_called_once()
 
 
 def test_on_toggle(qtbot, mocker):
     mock = mocker.stub()
-    switch = SwitchView()
+    switch = SwitchView("")
     switch.on_toggle(mock)
 
     qtbot.addWidget(switch)
-    qtbot.mousePress(switch, Qt.LeftButton)
+    qtbot.mouseClick(switch._button, Qt.LeftButton)
 
     mock.assert_called_once()
