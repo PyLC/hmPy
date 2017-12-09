4617ba2d37b9f9336f3d41ef7e2d3e9ff50bafe3
jseed <jonathan.j.seed@gmail.com>
2017-12-09 15:58:28 -0500

    Include SliderView in views module

--- hmpy/views/__init__.py
+++ hmpy/views/__init__.py
@@ -1,6 +1,7 @@
 from hmpy.views.button import ButtonView
 from hmpy.views.lcd import LCDView
 from hmpy.views.led import LEDView
+from hmpy.views.slider import SliderView
 from hmpy.views.view import View
 
-__all__ = ["ButtonView", "LCDView", "LEDView", "View"]
+__all__ = ["ButtonView", "LCDView", "LEDView", "SliderView", "View"]
