f825e45deed879469bd7393bec47a321d3fb6649
davidjoseph9 <davidjos3ph94@gmail.com>
2017-12-10 23:33:42 -0500

    Added progress bar view to views/_init_.py

--- hmpy/views/__init__.py
+++ hmpy/views/__init__.py
@@ -3,8 +3,10 @@ from hmpy.views.dial import DialView
 from hmpy.views.gauge import GaugeView
 from hmpy.views.lcd import LCDView
 from hmpy.views.led import LEDView
+from hmpy.views.progress_bar import ProgressBarView
 from hmpy.views.switch import SwitchView
 from hmpy.views.view import View
 from hmpy.views.chart import LineChartView
 
-__all__ = ["ButtonView", "DialView", "GaugeView", "LCDView", "LEDView", "LineChartView", "SwitchView", "View"]
+__all__ = ["ButtonView", "DialView", "GaugeView", "LCDView", "LEDView", "LineChartView", "ProgressBarView",
+           "SwitchView", "View"]
