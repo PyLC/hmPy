087164172a61fcf3231584ca25f56e0976d7a2f4
jseed <jonathan.j.seed@gmail.com>
2017-12-18 14:08:55 -0500

    Add setter methods for x/y scale to base View class

--- hmpy/views/view.py
+++ hmpy/views/view.py
@@ -8,9 +8,23 @@ class View(QWidget):
 
     def __init__(self, x_scale=1, y_scale=1, parent=None):
         super().__init__(parent)
-        x_size = self._DEFAULT_SIZE * x_scale
-        y_size = self._DEFAULT_SIZE * y_scale
+        self._x_scale = x_scale
+        self._y_scale = y_scale
+        self._resize()
+
+    def _resize(self):
+        x_size = self._DEFAULT_SIZE * self._x_scale
+        y_size = self._DEFAULT_SIZE * self._y_scale
         self._size = QSize(x_size, y_size)
+        self.resize(self._size)
+
+    def set_x_scale(self, scale):
+        self._x_scale = scale
+        self._resize()
+
+    def set_y_scale(self, scale):
+        self._y_scale = scale
+        self._resize()
 
     def sizeHint(self):
         return self.minimumSizeHint()
