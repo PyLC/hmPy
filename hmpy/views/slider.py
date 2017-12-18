7dbe857f13ce62dfe25d7b33695ef79ef1ed707e
lafo0203 <lafo0203@algonquinlive.com>
2017-12-18 00:27:09 -0500

    Merge branch 'Slider' of https://github.com/PyLC/hmPy into Slider
    # Conflicts:
    #	hmpy/interface.py
    #	hmpy/views/slider.py
    #	tests/test_interface.py

--- hmpy/views/slider.py
+++ hmpy/views/slider.py
@@ -2,26 +2,48 @@
 
 
 from .view import View
-from PyQt5.QtWidgets import QSlider,QLabel
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPainter
+from PyQt5.QtWidgets import QSlider
+from PyQt5.QtCore import Qt,QRect
+from PyQt5.QtGui import QPainter,QFont,QColor,QImage,QPixmap
 
 class SliderView(View):
     """The Slider view creates a slider for input.
 
     :param minimum: minimum int value of slider.
     :param maximum: maximum int value of slider.
+    :param current: starting value of slider.
     :param interval: distance each tick will travel on slider.
+    :param interactive: can user interact with it on UI.
+    :param fixed: width of slide.
     """
-    def __init__(self, minimum=0, maximum=100,interval=1):
+    def __init__(self ,minimum=0, maximum=100,current=0,interval=2,interactive=True,fixed=200):
         """Initialize the slider view."""
 
         super().__init__()
         self.slider = QSlider(self)
-        self.slider.setMinimum(minimum)
-        self.slider.setMaximum(maximum)
-        self.slider.setTickInterval(interval)
+        self.minimum_value = minimum;
+        self.maximum_value = maximum;
+        self.slider.setValue(current)
+        self.tick_interval = interval;
+        self.interactive = interactive
+        self.slider.setGeometry(self.x(),self.y(),800,200)
+        self.fixed_width=fixed
 
+    @property
+    def interactive(self):
+        return self.slider.isEnabled();
+
+    @interactive.setter
+    def interactive(self,interactive):
+        self.slider.setEnabled(interactive)
+
+    @property
+    def fixed_width(self):
+        return self.slider.width();
+
+    @fixed_width.setter
+    def fixed_width(self, width):
+        self.slider.setFixedWidth(width);
 
     @property
     def tick_interval(self):
@@ -40,10 +62,8 @@ class SliderView(View):
 
     @maximum_value.setter
     def maximum_value(self,maximum):
-        if maximum < 0:
             self.slider.setMaximum(self.slider.maximum())
-        else:
-            self.slider.setMaximum(maximum)
+
 
     @property
     def minimum_value(self):
@@ -51,10 +71,50 @@ class SliderView(View):
 
     @minimum_value.setter
     def minimum_value(self, minimum):
-        if minimum >= 0:
-            self.slider.setMinimum(minimum)
+        self.slider.setMinimum(minimum)
+
+
+    def paintEvent(self, event):
+
+        qp = QPainter()
+        rect = event.rect()
+
+        slider_rect = QRect(rect)
+
+        size = slider_rect.size()
+        center = slider_rect.center()
+
+
+        # Draw graphics on image first (double buffer)
+        image = QImage(size, QImage.Format_ARGB32_Premultiplied)
+        image.fill(0)
+
+        qp.begin(image)
+
+        self.drawText(event, qp)
+        qp.end()
+
+    # Draw image to widget
+        qp.begin(self)
+        qp.drawPixmap(1, 1, QPixmap.fromImage(image))
+        qp.end()
+
+    def drawText(self, event, qp):
+
+        qp.save()
+        rects = QRect(event.rect())
+        rects.setWidth(200)
+        rects.setHeight(40)
+        if (self.interactive):
+            qp.setPen(QColor(50, 50, 255))
         else:
-            self.slider.setMinimum(0)
+            qp.setPen(QColor(200, 200, 200))
+
+
+        qp.setFont(QFont('Decorative', 15))
+
+        qp.drawText(rects, Qt.AlignHCenter,str(self.slider.value()))
+        qp.restore()
 
     def set_horizontal_orientation(self):
         self.slider.setOrientation(Qt.Horizontal)
@@ -64,3 +124,4 @@ class SliderView(View):
 
     def on_change(self, callback):
         self.slider.valueChanged.connect(callback)
+
