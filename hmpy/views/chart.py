bc26541f903deaa9331a27af2494c36ca288bc6c
jseed <jonathan.j.seed@gmail.com>
2017-12-18 14:35:56 -0500

    Allow x and y to be scaled individually in LineChartView constructor

--- hmpy/views/chart.py
+++ hmpy/views/chart.py
@@ -15,8 +15,8 @@ class LineChartView(View):
     blue = Qt.blue
     yellow = Qt.yellow
 
-    def __init__(self, color=Qt.red, line_width=1, scale=1):
-        super().__init__(x_scale=scale, y_scale=scale)
+    def __init__(self, color=Qt.red, line_width=1, x_scale=1, y_scale=1):
+        super().__init__(x_scale=x_scale, y_scale=y_scale)
         self.chart = QChart()
         self.view = QChartView(self.chart, self)
         self.view.setRenderHint(QPainter.Antialiasing)
